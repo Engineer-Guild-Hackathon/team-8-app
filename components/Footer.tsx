142789105447d38d934667f2af52c54aef2220b7
kurrrru <kurrrru8850@gmail.com>
2025-09-11 23:08:45 +0900

    add Footer and Header

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+// components/Footer.tsx
+
+export const Footer = () => {
+  return (
+    <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+      <p className="text-muted-foreground">LearnGraph © 2025</p>
+    </footer>
+  );
+};
